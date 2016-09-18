c5ccb48002f715d786c13fd49109f03ca4df2352
nikki taliaferro <hello@nikkitaliaferro.com>
2016-09-18 12:31:16 -0400

    added priority radio buttons

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,9 @@
-angular.module('dewApp', []);
+var app = angular.module('dewApp', []);
+
+app.controller('MainCtrl', function($scope) {
+  $scope.priorityOption = [
+    {value: 1, text: "Low"},
+    {value: 2, text: "Medium"},
+    {value: 3, text: "High"}
+  ];
+});
