171a3f44f729fd538c18e524c0d313e01ec31333
nikki taliaferro <hello@nikkitaliaferro.com>
2016-12-28 10:13:16 -0800

    gh pages push

---
+++ scripts/directives/dTaskListNav.directive.js
@@ -0,0 +1,23 @@
+(function() {
+    angular
+        .module('app')
+        .directive('dTaskListNav', dTaskListNav);
+
+    function dTaskListNav() {
+        return {
+            restrict: 'E',
+            scope: {},
+            bindToController: true,
+            templateUrl: '/templates/task-list-nav.html',
+            controllerAs: '$ctrl',
+            controller: [TaskListNavCtrl]
+        };
+
+        // Global Variables
+        function TaskListNavCtrl() {
+            var vm = this;
+            
+        }
+    }
+
+})();
---
+++ scripts/directives/dTodoForm.directive.js
@@ -0,0 +1,71 @@
+(function() {
+    angular
+        .module('app')
+        .directive('dTodoForm', dTodoForm);
+
+    function dTodoForm() {
+        return {
+            restrict: 'E',
+            scope: {},
+            bindToController: true,
+            templateUrl: '/templates/form.html',
+            controllerAs: '$ctrl',
+            controller: ['$firebaseArray', TodoFormCtrl]
+        };
+
+        // Global Variables
+        function TodoFormCtrl($firebaseArray) {
+            var vm = this;
+            var taskRef = firebase.database().ref('tasks');
+            var taskList = $firebaseArray(taskRef);
+
+            console.log(taskList);
+
+            vm.taskList = taskList;
+
+            // Set Priority Options
+            vm.priorityOption = [{
+                value: 1,
+                text: "Low"
+            }, {
+                value: 2,
+                text: "Medium"
+            }, {
+                value: 3,
+                text: "High"
+            }];
+
+            // Create Task
+            vm.createTask = function() {
+                taskList.$add({
+                        taskPriority: vm.taskPriority,
+                        text: vm.taskText,
+                        isTaskComplete: false,
+                        category: 'active'
+                    })
+                    .then(
+                        vm.taskText = '',
+                        vm.isCreateTaskVisable = false
+                    );
+            };
+
+            // Is Completed Task
+            vm.isTaskComplete = function(tasks) {
+                if (tasks.isTaskComplete === false) {
+                    tasks.isTaskComplete = true;
+                    tasks.category = 'completed';
+                } else {
+                    tasks.isTaskComplete = false;
+                    tasks.category = 'active';
+                }
+                taskList.$save(tasks);
+            };
+
+            // Remove task
+            vm.removeTask = function(tasks) {
+                taskList.$remove(tasks);
+            };
+        }
+    }
+
+})();
